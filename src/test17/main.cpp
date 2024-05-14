7c373dc51c9ee380bf62ac39724a591fd0f97e8c
rafal11ck <76795799+rafal11ck@users.noreply.github.com>
2024-05-14 08:57:21 +0200

    hello world text

--- src/test17/main.cpp
+++ src/test17/main.cpp
@@ -3,6 +3,7 @@
  *@brief Rendering text
  **/
 #include "engine.hpp"
+#include <format>
 
 // #define GLT_MANUAL_VIEWPORT
 #define GLT_IMPLEMENTATION
@@ -17,15 +18,23 @@ int main() {
 
   // Creating text
   GLTtext *text = gltCreateText();
-  gltSetText(text, "Hello World!");
 
+  sf::Clock cl{};
   engine.setLoopFunction([&]() {
     // Begin text drawing (this for instance calls glUseProgram)
     gltBeginDraw();
 
+    const float time{cl.getElapsedTime().asSeconds()};
+
+    std::string textdata{"Hello World! " + std::format("{:.2f}", time)};
+
+    gltSetText(text, textdata.c_str());
+
     // Draw any amount of text between begin and end
     gltColor(1.0f, 1.0f, 1.0f, 1.0f);
-    gltDrawText2D(text, 40, 40, 1.0);
+    const auto size{engine.getWindow().getSize()};
+    gltDrawText2DAligned(text, size.x / 2.0, size.y / 2.0, 4, GLT_CENTER,
+                         GLT_CENTER);
 
     // Finish drawing text
     gltEndDraw();
